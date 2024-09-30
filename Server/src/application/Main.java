0fed8f9a8345ac470f65597c7866e4b8d312e6b8
nguyenduytay <duytayx8@gmail.com>
2024-09-30 15:02:31 +0700

    add

--- Server/src/application/Main.java
+++ Server/src/application/Main.java
@@ -1,20 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			 Parent root = FXMLLoader.load(getClass().getResource("/admin/interfaceAdmin.fxml"));
+			    primaryStage.initStyle(StageStyle.UNDECORATED);  // Tắt thanh tiêu đề
+		        primaryStage.setTitle("Hello World");
+		        primaryStage.setScene(new Scene(root));
+		        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
